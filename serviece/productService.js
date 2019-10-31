c303d2f95ff2e96d72c6ad2cc9947001d1d4670f
Phong Man <phongman5@gmail.com>
2019-10-31 14:40:15 +0700

    no cmt

--- serviece/productService.js
+++ serviece/productService.js
@@ -35,14 +35,14 @@ function getUserProductManger(username) {
   });
 }
 //create
-function createProduct(id, type, username,email, password) {
+function createProduct(id, username, password, email, type) {
   return User.create(
     {
-      id: id,
-      type: type,
+      id: id, 
       username: username,
+      password: password,
       email: email,
-      password: password
+      type: type
     },
     {
       raw: true
@@ -50,13 +50,13 @@ function createProduct(id, type, username,email, password) {
   );
 }
 //update
-function updateProduct(id, type, username, password,email) {
+function updateProduct(id, username, password, email, type) {
   return User.update(
-    {
-      type: type,
+    { 
       username: username,
       password: password,
-      email:email
+      email:email,
+      type: type
     },
     {
       where: {
